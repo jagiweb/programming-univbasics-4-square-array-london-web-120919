a84ed2acedb6b9d8aef940eddd964f1c644ec1b0
Javier Goncalves <jagi.ccs@gmail.com>
2019-11-27 13:33:19 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -2,9 +2,8 @@ def square_array(array)
   # your code here
   counter = 0
   while counter < array.size do
-    #new_array = Math.sqrt(array[counter])
-    #new_array
-    array.square!
+    new_array = Math.sqrt(array[counter])
+    new_array
     counter +=1
     end
 end
